e47dd344b10e07ce94e397f70df51a233e200b97
CNM <132835392+CNMarkham@users.noreply.github.com>
2026-08-30 16:02:26 -0400

    Making roateing when moving

--- Vannes-BB/Assets/Scripts/PlayerMove.cs
+++ Vannes-BB/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float movespeed;
+    public float pushForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector2.right * horizontal * movespeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Bounce Pad"))
+        {
+            Debug.Log("hit pad");
+            rb.AddForce (Vector2.up * pushForce, ForceMode2D.Impulse);
+        }
     }
 }
